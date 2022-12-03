d037b29bb976ae1143285fd570f090de2dc5b13d
AmineSSll <amine.sellami.a@gmail.com>
2022-12-03 23:21:21 +0100

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,149 +1,93 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from keras.layers import Layer, Conv2D, MaxPool2D, Flatten, Dense
+from keras.layers import Layer, Conv2D, MaxPool2D, Flatten, Dense, Input
 from train import *
 
+
+class PALModel(keras.Model):
     
-class PAL_model(keras.Model):
-    
-    def __init__(self, n_PAL_layer, weights_path, backbone = 'resnet50', ):
+    def __init__(self, weights_path = None,  num_classes = 7, backbone_name = 'vgg16', n_PAL_layer = 13 ):
         
         super().__init__()
+        self.num_classes = num_classes
         self.n_PAL_layer = n_PAL_layer
         self.weights_path = weights_path
+        self.im_shape = (224,224,3)
+        
         # Backbone CNN model choice
-        if backbone == 'vgg16':
+        if backbone_name == 'resnet50':
         
-            self.backbone = keras.applications.ResNet50()
+            self.backbone = [layer for layer in keras.applications.ResNet50(weights=self.weights_path
+                        ,include_top = False, input_shape = self.im_shape).layers]
         
-        # ResNet50 as default CNN backbone otherwise
+        # VGG16 as default CNN backbone otherwise
         else:
             
-            self.backbone = keras.applications.VGG16()
-
-        # build model with an input shape of 224,224,3 (image size)
+            self.backbone = [layer for layer in keras.applications.VGG16(weights=self.weights_path,
+                            include_top = False, input_shape = self.im_shape).layers]
+                
+        # Top part of model (classifier)
+        self.flat = Flatten()
+        self.dense1 = Dense(units=4096,activation="relu")
+        self.dense2 = Dense(units=4096,activation="relu")
+        self.classifier = Dense(units=7, activation="softmax")
         self.build((None, 224, 224, 3))
 
+
     def call(self, inputs):
+        
+        x = inputs
+        # We iterate over all layers to have a more visually appealing model.summary()
+        for layer in self.backbone:
+            x = layer(x)
             
-        return self.backbone(inputs)
-            
-    
+        # Chain the top of the model with the backbone
+        x = self.flat(x)
+        x = self.dense1(x)
+        x = self. dense2(x)
+        x = self.classifier(x)
+        
+        return x
+
+    # https://keras.io/guides/customizing_what_happens_in_fit/
     def train_step(self, data):
 
-        # Unpack the data. Its structure depends on your model and
-        # on what you pass to `fit()`.
+        # Unpack the data
         x, y = data
+        
+        # heatmap = heatmap(x)
     
         with tf.GradientTape() as tape:
+            
             y_pred = self(x, training=True)  # Forward pass
+        
+            
             # Compute the loss value
             # (the loss function is configured in `compile()`)
-            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+            
+            # Get PAL loss of n_PAL_layer (attribution map of nth layer)
+            PAL_loss(self.layers[self.n_PAL_layer], prior_heatmap, channels)
+            
+            # Output loss (categorical cross entropy in our case)
+            loss = self.compiled_loss(y, y_pred)
+            loss_total = PAL_loss + loss
     
         # Compute gradients
         trainable_vars = self.trainable_variables
-        gradients = tape.gradient(loss, trainable_vars)
+        gradients = x*tape.gradient(loss, trainable_vars)
         # Update weights
-        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        self.opt.apply_gradients(zip(gradients, trainable_vars))
         # Update metrics (includes the metric that tracks the loss)
         self.compiled_metrics.update_state(y, y_pred)
         # Return a dict mapping metric names to current value
         return {m.name: m.result() for m in self.metrics}
-            
-        
-    def test_step(self, data):
-        
-        x, y = data
-        
-        
-        
-# Changing grad behavior to input*grad, thus getting attribution of a layer
-class AttributionLayer(Conv2D):
     
-    
-    def __init__(self):
-        super(Attribution, self).__init__()
+    def compile(self, loss ='categorical_crossentropy', optimizer = 'Adam'):
         
-    # Calling gradient methods here ???
-    def call(self,inputs):
-        return grad_input(inputs, output_vector)
+        self.optimizer = optimizer
+        self.loss = loss
 
-class VGG16Attribution(keras.Model):
-
-  def __init__(self, n_PAL_layer):
-      
-    super(PALVGG16, self).__init__()
-    self.n_PAL_layer = n_PAL_layer
-    self.model = keras.Model()
-
-    self.conv1 = (Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-    self.conv2 = (Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-
-    self.mp1 = (MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-    self.conv3 = (Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-    self.conv4 = (Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-
-    self.mp2 = (MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-    self.conv5=(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-    self.conv6=(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-    self.conv7=(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-
-    self.mp3 = (MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-    self.conv8=(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    self.conv9=(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    self.conv10=(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-
-    self.mp4 = (MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-    self.conv11=(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    self.conv12=(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    self.conv13=(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-
-    self.mp5 = (MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-    self.flat = (Flatten())
-    self.dense1 = (Dense(units=4096,activation="relu"))
-    self.dense2 = (Dense(units=4096,activation="relu"))
-    self.dense3 = (Dense(units=2, activation="softmax"))
-
-
-  def call(self,inputs):
-
-    x = self.conv1(inputs)
-    x = self.conv2(x)
-
-    x = self.mp1(x)
-
-    x = self.conv3(x)
-    x = self.conv4(x)
-
-    x = self.mp2(x)
-
-    x = self.conv5(x)
-    x = self.conv6(x)
-    x = self.conv7(x)
-
-    x = self.mp4(x)
-
-    x = self.conv8(x)
-    x = self.conv9(x)
-    x = self.conv10(x)
-
-    x = self.mp5(x)
-
-    x = self.conv11(x)
-    x = self.conv12(x)
-    x = self.conv13(x)
-
-    x = self.flat(x)
-    x = self.dense1(x)
-    x = self.dense2(x)
-    x = self.dense3(x)
-    return x
+        pass
 
 
